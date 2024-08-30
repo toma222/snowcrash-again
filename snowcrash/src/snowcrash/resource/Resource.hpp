b8278000bf0cfff1c2858a4650ec8d2a0c98b5f4
toco4time <80077176+toco4time@users.noreply.github.com>
2024-08-29 20:42:47 -0500

    High key tweaking

--- snowcrash/src/snowcrash/resource/Resource.hpp
+++ snowcrash/src/snowcrash/resource/Resource.hpp
@@ -3,6 +3,7 @@
 
 #include <snowcrash/core/Core.hpp>
 #include <snowcrash/types/String.hpp>
+#include <snowcrash/types/Pair.hpp>
 
 namespace SC
 {
@@ -22,5 +23,25 @@ namespace SC
     {
     public:
         virtual void LoadResource(Resource **resource, String path) = 0;
+    };
+
+    // Puts resources into an array and then provides a function that loads them
+    struct ResourceArray
+    {
+    public:
+        using ResourcePair = Pair<Resource *, String>;
+
+        template <class R>
+        void AddResource(R *resourcePointer, String path)
+        {
+            m_resourceArray.Add(ResourcePair{
+                static_cast<Resource *>(resourcePointer),
+                path});
+        }
+
+        void LoadResources();
+
+    private:
+        ArrayList<ResourcePair> m_resourceArray;
     };
 } // namespace SC
