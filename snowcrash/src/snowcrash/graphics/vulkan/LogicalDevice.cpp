544c13986d30460c2a57f83ac6cf893c36462575
toco4time <80077176+toco4time@users.noreply.github.com>
2024-09-08 13:47:25 -0500

    added support for wireframe

--- snowcrash/src/snowcrash/graphics/vulkan/LogicalDevice.cpp
+++ snowcrash/src/snowcrash/graphics/vulkan/LogicalDevice.cpp
@@ -36,6 +36,8 @@ namespace SC
             VkPhysicalDeviceFeatures deviceFeatures{};
             deviceFeatures.samplerAnisotropy = VK_TRUE;
             deviceFeatures.sampleRateShading = VK_TRUE;
+            deviceFeatures.fillModeNonSolid = VK_TRUE;
+            deviceFeatures.wideLines = VK_TRUE;
 
             VkDeviceCreateInfo createInfo{};
             createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
